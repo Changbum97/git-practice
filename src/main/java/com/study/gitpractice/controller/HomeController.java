f95768fba0661e05f9b19bce40b4f43098d97db9
Changbum97 <chb2005@naver.com>
2022-11-06 18:17:00 +0900

    더하기 연산 결과 저장 및 출력

--- src/main/java/com/study/gitpractice/controller/HomeController.java
+++ src/main/java/com/study/gitpractice/controller/HomeController.java
@@ -4,13 +4,29 @@ import com.study.gitpractice.domain.Numbers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    List<Numbers> histories = new ArrayList<>();
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("numbers", new Numbers());
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String add(@ModelAttribute Numbers numbers, Model model) {
+        numbers.setAnswer(numbers.getX() + numbers.getY());
+        histories.add(numbers);
+        model.addAttribute("numbers", numbers);
+        model.addAttribute("histories", histories);
         return "home";
     }
 }
